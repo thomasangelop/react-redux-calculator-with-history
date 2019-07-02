597f929e98ee9ef72350be8f1fe6aceed8e5c791
thomasangelop <me@thomasagnelo.codes>
2019-07-02 13:47:34 -0500

    server file setup

---
+++ server/server.js
@@ -0,0 +1,85 @@
+const mongoose = require('mongoose');
+const express = require('express');
+var cors = require('cors');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const Data = require('./data');
+
+const API_PORT = 3001;
+const app = express();
+app.use(cors());
+const router = express.Router();
+
+//monogo database
+const dbRoute = '';
+
+//connects backend code with the database
+mongoose.connect(dbRoute, { useNewUrlParser: true });
+
+let db = mongoose.connection;
+
+db.once('open', () => console.log('connected to the database'));
+
+//checks if the connection with the database is successful
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// bodyParser, parses the request body to be a readable json format
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(logger('dev'));
+
+// get method
+// this method fetches all available data in our database
+router.get('/getData', (req, res) => {
+  Data.find((err, data) => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true, data: data });
+  });
+});
+
+// update method
+// this method overwrites existing data in our database
+router.post('/updateData', (req, res) => {
+  const { id, update } = req.body;
+  Data.findByIdAndUpdate(id, update, (err) => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true });
+  });
+});
+
+// delete method
+// this method removes existing data in our database
+router.delete('/deleteData', (req, res) => {
+  const { id } = req.body;
+  Data.findByIdAndRemove(id, (err) => {
+    if (err) return res.send(err);
+    return res.json({ success: true });
+  });
+});
+
+// create method
+// this method adds new data in our database
+router.post('/putData', (req, res) => {
+  let data = new Data();
+
+  const { id, message } = req.body;
+
+  if ((!id && id !== 0) || !message) {
+    return res.json({
+      success: false,
+      error: 'INVALID INPUTS',
+    });
+  }
+  data.message = message;
+  data.id = id;
+  data.save((err) => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true });
+  });
+});
+
+// append /api for our http requests
+app.use('/api', router);
+
+// launch our backend into a port
+app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
